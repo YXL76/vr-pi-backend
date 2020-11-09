245636d892e34dc589e2c8b1f67165bc6b727829
YXL <chenxin.lan.76@gmail.com>
2020-11-09 20:47:02 +0800

    🦄refactor: simplified formula

--- src/server.rs
+++ src/server.rs
@@ -166,11 +166,13 @@ fn data_processing(data: &str) -> Result<String> {
     let a = 5.71559214e-05; // 与舵机相匹配
     let b = 3.60082305e-02; // 与舵机相匹配
     let c = 2.50000000;
-    let direction = 90.0;
+    // let direction = 90.0;
     let rotation: Rotation = serde_json::from_str(data)?;
 
-    let vertical_direction = direction - (rotation.gamma + PI / 2.0) / (PI / 2.0) * 90.0;
-    let level_direction = direction - (rotation.alpha + PI / 2.0) / (PI / 2.0) * 90.0;
+    let vertical_direction = rotation.gamma * -180.0 / PI;
+    // let vertical_direction = direction - (rotation.gamma + PI / 2.0) / (PI / 2.0) * 90.0;
+    let level_direction = rotation.alpha * -180.0 / PI;
+    // let level_direction = direction - (rotation.alpha + PI / 2.0) / (PI / 2.0) * 90.0;
     let level_direction = 180.0 - level_direction;
 
     let vertical_duty = a * vertical_direction * vertical_direction + b * vertical_direction + c;
