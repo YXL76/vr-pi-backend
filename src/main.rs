13a25c3ed61533f7b35a14445b7a66a459ca52e4
YXL <chenxin.lan.76@gmail.com>
2020-12-12 00:11:05 +0800

    modify listen address

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,7 @@ async fn main() -> std::io::Result<()> {
             // static resources
             .service(fs::Files::new("/static/", "static/"))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
